fd708d7c4ae09db63259eb580ddd660922c02d95
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2015-02-19 16:58:34 +0100

    cleanup

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -4,28 +4,8 @@ namespace UsefulTests\Tests;
 use \Useful;
 
 
-class Int{
-    private $i;
-    public function __construct($i)
-    {
-
-    }
-}
 
 class FirstTest extends \PHPUnit_Framework_TestCase {
-
-    public function test_N_times_should_return_iterator()
-    {
-//        Useful\Useful::load();
-//        $this->assertInstanceOf('Useful\Iterator', \Useful\N(3));
-    }
-//
-//    public function test_fail()
-//    {
-//        $result = N::times(3)->run(function(){return 1;});
-//        $this->assertEquals([1, 1, 1], $result);
-//    }
-
 
     public function test_N()
     {
